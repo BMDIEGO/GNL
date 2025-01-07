ed660c3f15f2efc8bdc5734362953225be645fd1
BMDIEGO <102241236+BMDIEGO@users.noreply.github.com>
2025-01-07 12:43:41 +0100

    Add files via upload

---
+++ gnl/get_next_line_utils_bonus.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbarrajo <dbarrajo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/10 12:35:21 by marvin            #+#    #+#             */
+/*   Updated: 2024/12/23 13:14:29 by dbarrajo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+// Une dos cadenas de caracteres (s1 y s2) en una nueva cadena.
+char	*ft_strjoin_gnl(char *s1, char *s2)
+{
+	int		tamanototal;
+	char	*resultado;
+	int		i;
+	int		j;
+
+	i = 0;
+	tamanototal = ft_strlen(s1) + ft_strlen(s2);
+	resultado = malloc((tamanototal + 1));
+	if (!resultado || !s1 || !s2)
+		return (NULL);
+	while (s1[i] != 0)
+	{
+		resultado[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j] != 0)
+	{
+		resultado[i] = s2[j];
+		i++;
+		j++;
+	}
+	resultado[tamanototal] = 0;
+	free(s1);
+	return (resultado);
+}
+
+// Calcula la longitud de una cadena de caracteres.
+size_t	ft_strlen(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+// Asigna memoria para un array de nmemb elementos de tamaño size.
+char	*ft_calloc_gnl(size_t nmemb, size_t size)
+{
+	char	*resultado;
+	size_t	i;
+	size_t	longitud;
+
+	longitud = size * nmemb;
+	resultado = malloc(longitud);
+	if (!resultado)
+		return (NULL);
+	i = 0;
+	while (i < longitud)
+	{
+		resultado[i] = 0;
+		++i;
+	}
+	return (resultado);
+}
+
+// Busca el primer carácter en la cadena s y devuelve un puntero a esa posición.
+char	*ft_strchr(char *s, int c)
+{
+	char	*str;
+
+	str = s;
+	while (*str != c && *str != 0)
+		str++;
+	if (*str == c)
+		return (str);
+	else
+		return (NULL);
+}
---
+++ gnl_test/gnl_test/get_next_line.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/10 12:35:33 by marvin            #+#    #+#             */
+/*   Updated: 2024/12/12 17:17:07 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h> // Para funciones de asignación de memoria y NULL
+# include <sys/types.h> // Para tipos de datos como size_t
+# include <sys/uio.h> // Para la función read
+# include <unistd.h> // Para funciones del sistema como read y close
+# include <fcntl.h> // Para la función open
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42 // Define el tamaño del buffer si no está definido
+# endif
+
+// Prototipos de funciones
+char	*get_next_line(int fd); // Función principal para obtener la siguiente línea de un archivo
+char	*ft_strjoin_gnl(char *s1, char *s2); // Une dos cadenas de caracteres
+char	*ft_strchr(char *s, int c); // Busca el primer carácter c en la cadena s
+char	*ft_calloc_gnl(size_t nmemb, size_t size); // Asigna memoria para un array y la inicializa a cero
+size_t	ft_strlen(char *s); // Calcula la longitud de una cadena de caracteres
+
+#endif
